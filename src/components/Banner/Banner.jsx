a025930b4f34615d93231dc9d76e9bfe2de6b094
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-20 06:32:07 +0000

    Setting all the gallery pictures

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 
 import './Banner.css';
+import images from '../../constants/images';
 
 
 
 const Banner = () => {
   return (
     <div className='app__banner'>
-        <div className='text-content'>
-            <h1>I create products that make profit</h1>
-            <p>
-                I will make a product that will bring<br /> customers and make you stand out in the market
-            </p>
-            <a href="#">Order Now</a>
+      <div className='text-content'>
+          <h1>I create products that make profit</h1>
+          <p>
+              I will make a product that will bring<br /> customers and make you stand out in the market
+          </p>
+          <a href="#">Order Now</a>
+      </div>
+
+      <div className="gallery-container">
+
+        <div className="image image-a">
+          <img src={ images.kech1 } alt='Marrakech Old Medina' draggable={ false } />
         </div>
+        <div className="image image-b">
+          <img src={ images.kech2 } alt='Marrakech Old Medina' draggable={ false } />
+        </div>
+        <div className="image image-c">
+          <img src={ images.kech3 } alt='Marrakech Old Medina' draggable={ false } />
+        </div>
+
+        <div className="image image-d">
+          <img src={ images.dogs1 } alt='Dogs' draggable={ false } />
+        </div>
+        <div className="image image-e">
+          <img src={ images.dogs2 } alt='Dogs' draggable={ false } />
+        </div>
+
+        <div className="image image-f">
+          <img src={ images.water1 } alt='Water' draggable={ false } />
+        </div>
+        <div className="image image-g">
+          <img src={ images.water2 } alt='Water' draggable={ false } />
+        </div>
+
+      </div>
+
     </div>
   );
 };
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,23 @@
 import profile from '../assets/profile.jpg';
 
+import dogs1   from '../assets/dogs/1.jpg';
+import dogs2   from '../assets/dogs/2.jpg';
+
+import kech1   from '../assets/marrakech/1.jpg';
+import kech2   from '../assets/marrakech/2.jpg';
+import kech3   from '../assets/marrakech/3.jpg';
+
+import water1  from '../assets/water/1.jpg';
+import water2  from '../assets/water/2.jpg';
+
 const images = {
     profile,
+
+    dogs1, dogs2,
+
+    kech1, kech2, kech3,
+    
+    water1, water2,
 };
 
 export default images;
