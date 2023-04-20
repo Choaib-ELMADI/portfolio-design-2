2ed180e40297645dcae3f808f42936221837d96e
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-20 07:49:36 +0000

    Adding keys

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -19,36 +19,33 @@ const Banner = () => {
       <div className="gallery-container">
 
         <div className="image image-a">
-          <img src={ images.electro1 } alt='Electronics' draggable={ false } />
+          <img key='electronics-aa' src={ images.electro1 } alt='Electronics' draggable={ false } />
         </div>
         <div className="image image-b">
-          <img src={ images.electro2 } alt='Electronics' draggable={ false } />
+          <img key='electronics-bb' src={ images.electro2 } alt='Electronics' draggable={ false } />
         </div>
         <div className="image image-c">
-          <img src={ images.electro3 } alt='Electronics' draggable={ false } />
+          <img key='electronics-cc' src={ images.electro3 } alt='Electronics' draggable={ false } />
         </div>
 
         <div className="image image-1">
-          <img src={ images.code3 } alt='Coding' draggable={ false } />
+          <img key='coding-1' src={ images.code3 } alt='Coding' draggable={ false } />
         </div>
         <div className="image image-2">
-          <img src={ images.code2 } alt='Coding' draggable={ false } />
+          <img key='coding-11' src={ images.code2 } alt='Coding' draggable={ false } />
         </div>
         <div className="image image-3">
-          <img src={ images.code1 } alt='Coding' draggable={ false } />
+          <img key='coding-111' src={ images.code1 } alt='Coding' draggable={ false } />
         </div>
 
         <div className="image image-A">
-          <img src={ images.model1 } alt='3D Modeling' draggable={ false } />
-        </div>
-        <div className="image image-B">
-          <img src={ images.model2 } alt='3D Modeling' draggable={ false } />
+          <img key='modeling-1' src={ images.model1 } alt='3D Modeling' draggable={ false } />
         </div>
         <div className="image image-C">
-          <img src={ images.model3 } alt='3D Modeling' draggable={ false } />
+          <img key='modeling-2' src={ images.model3 } alt='3D Modeling' draggable={ false } />
         </div>
         <div className="image image-D">
-          <img src={ images.model4 } alt='3D Modeling' draggable={ false } />
+          <img key='modeling-3' src={ images.model4 } alt='3D Modeling' draggable={ false } />
         </div>
 
       </div>
