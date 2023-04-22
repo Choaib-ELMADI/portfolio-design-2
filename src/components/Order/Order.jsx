eca51ef62aec1815ddd5caa6f4f173f5555b892c
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-22 12:25:22 +0000

    Move to the last section

---
+++ src/components/Order/Order.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Order.css';
+
+
+
+const Order = () => {
+  return (
+    <div className='app__order'>
+        <h1>Order Development</h1>
+        <p>
+            Let's create a product together that will bring profit to 
+            your business and set you apart from the competition.
+        </p>
+        <a href="#">Order Now</a>
+        <div className='pics-container'>
+            
+        </div>
+    </div>
+  );
+};
+
+export default Order;
